3e64fb69561e2310198c7a1c0cfc1e497c0a02fb
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-02-08 17:31:46 -0500

    creacion de microservicios

---
+++ microservices-course/src/main/java/com/microservices/course/MicroservicesCourseApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCourseApplication.class, args);
+	}
+
+}
